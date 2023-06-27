e3d2ad44aeefe2114706e0943b97268eefcf4388
SasaniP <sasasulakshi@gmail.com>
2023-06-27 13:27:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sqlite_database/Adapter.java
@@ -0,0 +1,56 @@
+package com.example.sqlite_database;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    private Context context;
+    private ArrayList name_id,_mail_id,age_id;
+
+    public Adapter(Context context, ArrayList name_id, ArrayList _mail_id, ArrayList age_id) {
+        this.context = context;
+        this.name_id = name_id;
+        this._mail_id = _mail_id;
+        this.age_id = age_id;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.userdata,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name_id.setText(String.valueOf(name_id.get(position)));
+        holder.email_id.setText(String.valueOf(_mail_id.get(position)));
+        holder.age_id.setText(String.valueOf(age_id.get(position)));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return name_id.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name_id,email_id,age_id;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name_id=itemView.findViewById(R.id.name_text);
+            email_id=itemView.findViewById(R.id.mail_text);
+            age_id=itemView.findViewById(R.id.age_text);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/sqlite_database/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.sqlite_database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name,mail,age;
+    Button insert_btn,view_btn;
+    DBHepler db;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.name_ed);
+        mail=findViewById(R.id.mail_ed);
+        age=findViewById(R.id.age_ed);
+        insert_btn=findViewById(R.id.insert_btn);
+        view_btn=findViewById(R.id.view_btn);
+
+        db=new DBHepler(this);
+
+
+        view_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,UserList.class));
+            }
+        });
+        insert_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name_text=name.getText().toString();
+                String email_text=mail.getText().toString();
+                String age_text=age.getText().toString();
+
+
+                Boolean  checkinsertData=db.insertuserData(name_text,email_text,age_text);
+                if (checkinsertData==true){
+                    Toast.makeText(MainActivity.this, "New Data Inserted", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "new Data Not Inserted!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
